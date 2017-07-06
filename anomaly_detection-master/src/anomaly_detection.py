7e6728d142506a44b22cc493e1227ad5baaeb23e
yidizhang <zcj1022@hotmail.com>
2017-07-06 00:07:39 -0400

    Add files via upload

--- anomaly_detection-master/src/anomaly_detection.py
+++ anomaly_detection-master/src/anomaly_detection.py
@@ -1,6 +1,5 @@
 import sys
 import json
-import itertools
 from collections import deque
 import numpy as ny
 
@@ -25,15 +24,6 @@ def find_friends(Graph,start,num):
 
     
     return visited
-
-# the function to generate all edges from social network graph
-def generate_edges(graph):
-    edges = []
-    for node in graph:
-        for neighbour in graph[node]:
-            edges.append((node, neighbour))
-
-    return edges
 
 # the function to compute mean of last T purchases within D-th degree social network
 def compute_mean(purchase_history,find_friends, T):
@@ -73,10 +63,6 @@ def check_anomaly(event,mean, sd):
 
 def printUsage():
     print ("Usage: python average_degree.py <input_file1> <input_file2> <output_file>")  # output_file here is always flagged_purchase.json
-
-def getHashTagLinks(tags):
-    return itertools.permutations(tags,2)
-
            
 # the function to read batch_log file to generate initial social graph and purchase history, then it reads stream_log file to find anomaly and flag the event in output file
 # the social graph and purchase history is also updated. 
@@ -98,21 +84,22 @@ def generateGraph(inputTweets,inputTweets1):
         # add to social graph with event_type befriend
                   
             if t["event_type"]=="befriend":
-                addEdges = getHashTagLinks((t["id1"],t["id2"]))
-                for edge in addEdges:
-                    if edge[0] in social_graph:
-                        social_graph[edge[0]].append(edge[1])
-                    else:
-                        social_graph[edge[0]] = [edge[1]]
+                if t["id1"] in social_graph:
+                    social_graph[t["id1"]].append(t["id2"])
+                else:
+                    social_graph[t["id1"]] = [t["id2"]]
+                if t["id2"] in social_graph:
+                    social_graph[t["id2"]].append(t["id1"])
+                else:
+                    social_graph[t["id2"]]= [t["id1"]]
+                                             
         # remove from social graph with event_tpye unfriend
             if t["event_type"]=="unfriend":
-                addEdges = getHashTagLinks((t["id1"],t["id2"]))
-                for edge in addEdges:
-                    if edge[0] in social_graph and edge[1] in social_graph:
-                        if edge[1] in social_graph[edge[0]]:
-                            social_graph[edge[0]].remove(edge[1])
-                        if edge[0] in social_graph[edge[1]]:
-                            social_graph[edge[1]].remove(edge[0])
+                if t["id1"] in social_graph and t["id2"] in social_graph:
+                    if t["id1"] in social_graph[t["id2"]]:
+                        social_graph[t["id2"]].remove(t["id1"])
+                    if t["id2"] in social_graph[t["id1"]]:
+                        social_graph[t["id1"]].remove(t["id2"])
                
 #begin reading stream_log.json file
     for t in inputTweets1:
@@ -122,32 +109,35 @@ def generateGraph(inputTweets,inputTweets1):
         
         # keep record of the purchase history and store it as a list of tuples.
         if t["event_type"]=="purchase":
-            edges = generate_edges(social_graph)                                
+                                            
             mean = compute_mean(purchase_list,find_friends(social_graph,t['id'],int(D)),int(T))[1]
             amount_list = compute_mean(purchase_list,find_friends(social_graph,t['id'],int(D)),int(T))[0]
-            sd = compute_sd(mean, amount_list)                
+            sd = compute_sd(mean, amount_list)
+            print ("sd is :", sd)
             detection = check_anomaly(t,mean,sd)
             f = open('flagged_purchases.json', 'w') # open for 'w'riting
             f.write(str(detection))
             purchase_list.append((t["id"],t["timestamp"],t["amount"]))                   
         
         # add to social graph with event_type befriend
-        if t["event_type"]=="befriend":
-            addEdges = getHashTagLinks((t["id1"],t["id2"]))
-            for edge in addEdges:
-                if edge[0] in social_graph:
-                    social_graph[edge[0]].append(edge[1])
+                  
+            if t["event_type"]=="befriend":
+                if t["id1"] in social_graph:
+                    social_graph[t["id1"]].append(t["id2"])
                 else:
-                    social_graph[edge[0]] = [edge[1]]
+                    social_graph[t["id1"]] = [t["id2"]]
+                if t["id2"] in social_graph:
+                    social_graph[t["id2"]].append(t["id1"])
+                else:
+                    social_graph[t["id2"]]= [t["id1"]]
+                                             
         # remove from social graph with event_tpye unfriend
-        if t["event_type"]=="unfriend":
-            addEdges = getHashTagLinks((t["id1"],t["id2"]))
-            for edge in addEdges:
-                if edge[0] in social_graph and edge[1] in social_graph:
-                    if edge[1] in social_graph[edge[0]]:
-                        social_graph[edge[0]].remove(edge[1])
-                    if edge[0] in social_graph[edge[1]]:
-                        social_graph[edge[1]].remove(edge[0])                                       
+            if t["event_type"]=="unfriend":
+                if t["id1"] in social_graph and t["id2"] in social_graph:
+                    if t["id1"] in social_graph[t["id2"]]:
+                        social_graph[t["id2"]].remove(t["id1"])
+                    if t["id2"] in social_graph[t["id1"]]:
+                        social_graph[t["id1"]].remove(t["id2"])                                      
 
 def main(argv):
     
